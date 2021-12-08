42e70fc85323da95ecfacbaeb825247608454420
weton01 <wellington_ferreira280@hotmail.com>
2021-12-08 12:54:54 -0300

    feature: add the first files

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,8 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+  test('should ', () => {
+    
+  });
+});
